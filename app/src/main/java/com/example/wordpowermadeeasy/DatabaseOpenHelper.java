dfc1a306af3570877b87ffca170329f72386bb8c
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-08-28 03:01:17 +0530

    Word Power Made Easy hereby open sourced - Let there be Word Power !

---
+++ app/src/main/java/com/example/wordpowermadeeasy/DatabaseOpenHelper.java
@@ -0,0 +1,76 @@
+/**
+ *   Word Power Made Easy - A vocabulary building application
+ *
+ *   This file is part of LogMeIn.
+ *   Copyright (c) 2014 Shubham Chaudhary <me@shubhamchaudhary.in>
+ *   Copyright (c) 2014 Tanjot Kaur <tanjot28@gmail.com>
+ *   Copyright (c) 2014 Vivek Aggarwal <vivekaggarwal92@gmail.com>
+ *
+ *   This file is now also a part of WordPowerMadeEasy.
+ *   Copyright (c) 2014 Shubham Chaudhary <me@shubhamchaudhary.in>
+ *
+ *   WordPowerMadeEasy is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   WordPowerMadeEasy is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with WordPowerMadeEasy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.example.wordpowermadeeasy;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+//import android.provider.BaseColumns;
+
+
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "wordManager";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_WORDLIST = "wordList";
+    public static final String KEY_INDEX = "index";
+    public static final String KEY_WORD = "word";
+    public static final String KEY_MEANING = "meaning";
+    public static final String KEY_SCORE = "score";
+    private static final String TAG = DatabaseOpenHelper.class.getSimpleName();
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Creating tables
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_WORDLIST + " ("
+                + KEY_INDEX + " INTEGER PRIMARY KEY," + KEY_WORD + " TEXT,"
+                + KEY_MEANING + " TEXT," + KEY_SCORE + " INTEGER" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //called when database is upgraded like modifying the table structure,
+        //adding constraints to database etc
+        if (oldVersion == 1) {
+            //alter current table or schema and execute the sql command using db.execSQL(sql)
+            this.onUpgrade(db, ++oldVersion, newVersion);
+        }
+        /* Alternative way to handle this
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+
+        // Create tables again
+        onCreate(db);
+         */
+    }
+}
+
+// vim: set ts=4 sw=4 tw=0 et :
---
+++ app/src/main/java/com/example/wordpowermadeeasy/MainActivity.java
@@ -0,0 +1,124 @@
+/**
+ *   Word Power Made Easy - A vocabulary building application
+ *
+ *   Copyright (c) 2014 Shubham Chaudhary <me@shubhamchaudhary.in>
+ *
+ *   This file is part of WordPowerMadeEasy.
+ *
+ *   WordPowerMadeEasy is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   WordPowerMadeEasy is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with WordPowerMadeEasy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.example.wordpowermadeeasy;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+    //Class Variables
+    TextView textview_word, textview_meaning;
+    Button button_next;
+
+    /* Engines */
+    WordEngine wordEngine;
+    // DatabaseEngine databaseEngine;
+    int incrementor;
+    SharedPreferences mPrefs;
+    final String welcomeScreenShownPref = "welcomeScreenShown";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wordEngine = new WordEngine(this);
+//		databaseEngine = new DatabaseEngine(this);
+//		databaseEngine.populateDatabase(wordEngine.getMapFromXml());
+
+        textview_word = (TextView)findViewById(R.id.textview_word);
+        textview_meaning = (TextView)findViewById(R.id.textview_meaning);
+
+        this.incrementor = 0;
+
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // second argument is the default to use if the preference can't be found
+        Boolean welcomeScreenShown = mPrefs.getBoolean(welcomeScreenShownPref, false);
+
+        if (!welcomeScreenShown) {
+            showWelcomeScreen();
+            SharedPreferences.Editor editor = mPrefs.edit();
+            editor.putBoolean(welcomeScreenShownPref, true);
+            editor.apply(); // Very important to save the preference
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** My Functions */
+
+    public void nextRandom(View v) throws  XmlPullParserException, IOException {
+        //wordEngine.getWeekPairs(this.incrementor++);
+        WordPair wordPair = nextRandom();
+        textview_word.setText(wordPair.getWord());
+        textview_meaning.setText(wordPair.getMeaning());
+    }
+
+    public WordPair nextRandom(){ return wordEngine.getRandomWord(); }
+
+
+    private void showWelcomeScreen(){
+        // here you can launch another activity if you like
+        // the code below will display a popup
+        String welcomeTitle = getResources().getString(R.string.welcomeTitle);
+        String welcomeText = getResources().getString(R.string.welcomeText);
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_menu_help).setTitle(welcomeTitle).setMessage(welcomeText).setPositiveButton(
+                R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+    }
+}
+// vim: set ts=4 sw=4 tw=0 noet :
