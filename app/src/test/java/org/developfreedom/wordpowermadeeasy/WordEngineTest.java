c882a6b5854349ceb477a0009271072c76499f60
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-03-22 00:50:53 +0530

    Added robolectric plugins

---
+++ app/src/test/java/org/developfreedom/wordpowermadeeasy/WordEngineTest.java
@@ -0,0 +1,18 @@
+package org.developfreedom.wordpowermadeeasy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+
+public class WordEngineTest {
+
+    @Test
+    public void testTrueIsTrue() {
+        assertEquals(true, true);
+    }
+
+}
