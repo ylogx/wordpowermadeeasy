d7b66232e3779f96b758a22fae91aae6e4208c46
Shubham Chaudhary <me@shubhamchaudhary.in>
2016-02-22 02:27:45 +0530

    Cleanup word engine, Remove unused code

--- app/src/main/java/org/developfreedom/wordpowermadeeasy/word/WordEngine.java
+++ app/src/main/java/org/developfreedom/wordpowermadeeasy/word/WordEngine.java
@@ -23,13 +23,11 @@ package org.developfreedom.wordpowermadeeasy.word;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.res.XmlResourceParser;
-import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-import org.developfreedom.wordpowermadeeasy.storage.DatabaseOpenHelper;
 import org.developfreedom.wordpowermadeeasy.R;
+import org.developfreedom.wordpowermadeeasy.storage.DatabaseOpenHelper;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -37,83 +35,60 @@ import java.io.IOException;
 import java.util.*;
 
 public class WordEngine {
+    public static final int DEFAULT_SCORE = 50;
     private static int count = 0;
     private final Context context;
     private final SQLiteOpenHelper myDatabaseHelper;
     private SQLiteDatabase database;
-    private Cursor cursor;
-    private Map<String, String> word_map;
+    private Map<String, String> wordMap;
 
     public WordEngine(Context ctx) {
         this.context = ctx;
         this.myDatabaseHelper = new DatabaseOpenHelper(context);
         try {
             //XXX do this once ever
-            this.word_map = readXml();
-            populateDatabase(word_map);
+            this.wordMap = readXml();
+            populateDatabase();
             WordEngine.count += 1;
-            System.out.println("Count ");
-            System.out.println(WordEngine.count);
-        } catch (XmlPullParserException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+            Log.i("WordEngine", "Count: " + count);
+        } catch (XmlPullParserException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void populateDatabase(Map<String, String> word_map) {
+    private void populateDatabase() {
         try {
-            int DEFAULT_SCORE = 50;
-            String word, meaning;
-            List<String> keys = new ArrayList<String>(word_map.keySet());
+            database = myDatabaseHelper.getWritableDatabase();
+            for (Map.Entry<String, String> wordMeaningEntry : wordMap.entrySet()) {
+                try {
+                    ContentValues values = new ContentValues();
+                    values.put(DatabaseOpenHelper.KEY_WORD, wordMeaningEntry.getKey());
+                    values.put(DatabaseOpenHelper.KEY_MEANING, wordMeaningEntry.getValue());
+                    values.put(DatabaseOpenHelper.KEY_SCORE, DEFAULT_SCORE);
 
-            for (String key : keys) {
-                word = key;
-                meaning = word_map.get(word);
-
-                database = myDatabaseHelper.getWritableDatabase();
-                ContentValues values = new ContentValues();
-//              values.put(DatabaseOpenHelper.KEY_INDEX, i);
-                values.put(DatabaseOpenHelper.KEY_WORD, word);
-                values.put(DatabaseOpenHelper.KEY_MEANING, meaning);
-                values.put(DatabaseOpenHelper.KEY_SCORE, DEFAULT_SCORE);
-
-                database.insert(DatabaseOpenHelper.TABLE_WORDLIST, null, values);
-                database.close();   //FIXME Is it right to open and close everytime
-            }//end for loop
+                    database.insert(DatabaseOpenHelper.TABLE_WORDLIST, null, values);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }//end populateDatabase
-
-
-    private void check_database() {
-        try {
-            //Checking db
-            String[] columns = {
-//                      DatabaseOpenHelper.KEY_INDEX,
-                    DatabaseOpenHelper.KEY_WORD,
-                    DatabaseOpenHelper.KEY_MEANING,
-                    DatabaseOpenHelper.KEY_SCORE
-            };
-            cursor = database.query(DatabaseOpenHelper.TABLE_WORDLIST, columns,
-                    null, null, null, null, null);
-            Log.v("DE", "Cursor Object" + DatabaseUtils.dumpCursorToString(cursor));
-            Log.d("DE", "database connected and values inserted with primary key");
-            database.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } finally {
+            if (database != null) {
+                try {
+                    database.close();
+                } catch (Exception ignored) {
+                }
+            }
         }
     }
+
 
     public WordPair getRandomWord() {
         Random rand = new Random();
-        List<String> keys = new ArrayList<String>(word_map.keySet());
+        List<String> keys = new ArrayList<String>(wordMap.keySet());
         String randomKey = keys.get(rand.nextInt(keys.size()));
-        String value = word_map.get(randomKey);
-        //WordPair new_word = new WordPair(randomKey, value);
+        String value = wordMap.get(randomKey);
         return new WordPair(randomKey, value);
     }
 
@@ -122,77 +97,42 @@ public class WordEngine {
 
         xrp.next();
         int eventType = xrp.getEventType();
-        String word = null, meaning = null;
-        Map<String, String> word_pair_map = new HashMap<String, String>();
+        String word = null;
+        String meaning = null;
+        Map<String, String> wordPairMap = new HashMap<>();
         while (eventType != XmlPullParser.END_DOCUMENT) {
-            if (eventType == XmlPullParser.START_TAG
-                    && xrp.getName().equalsIgnoreCase("pair")) {
+            if ((eventType == XmlPullParser.START_TAG)
+                    && "pair".equalsIgnoreCase(xrp.getName())) {
                 //NOTE: In our xml file meaning comes before word and that matters
                 eventType = xrp.next();
-                if (eventType == XmlPullParser.START_TAG
-                        && xrp.getName().equalsIgnoreCase("meaning")) {
+                if ((eventType == XmlPullParser.START_TAG)
+                        && "meaning".equalsIgnoreCase(xrp.getName())) {
                     eventType = xrp.next();
                     if (eventType == XmlPullParser.TEXT) {
                         //TODO: Handle situation when either word or meaning is missing
                         meaning = xrp.getText();
-//                      Log.i("M","Meaning: "+meaning);
                     }
                 }
                 xrp.next();                 //End tag meaning
                 eventType = xrp.next();     //Start tag word
-                if (eventType == XmlPullParser.START_TAG
-                        && xrp.getName().equalsIgnoreCase("word")) {
+                if ((eventType == XmlPullParser.START_TAG)
+                        && "word".equalsIgnoreCase(xrp.getName())) {
                     eventType = xrp.next();
                     if (eventType == XmlPullParser.TEXT) {
                         word = xrp.getText();
-//                      Log.i("W","Word: "+word);
                     }
-                }//end if elif
-            }//end if START_TAG
-            else if (eventType == XmlPullParser.END_TAG && xrp.getName().equalsIgnoreCase("pair")) {
-                if (word != null && meaning != null && word.length() != 0 && meaning.length() != 0) {
-                    if (word_pair_map.get(word) == null) {
-//                      Log.d("map",word+meaning);
-                        word_pair_map.put(word, meaning);
+                }
+            } else if ((eventType == XmlPullParser.END_TAG)
+                    && "pair".equalsIgnoreCase(xrp.getName())) {
+                if ((word != null) && (meaning != null) && (word.length() != 0) && (meaning.length() != 0)) {
+                    if (wordPairMap.get(word) == null) {
+                        wordPairMap.put(word, meaning);
                     }
                 }
             }
             eventType = xrp.next();
-        }//end while
-        return word_pair_map;
-    }//end readXml
-
-    private Map<String, String> getMapFromXml() {
-        try {
-            return readXml();
-        } catch (XmlPullParserException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         }
-        return null;
+        return wordPairMap;
     }
-
-    private List<WordPair> getWeekPairs(int week_number) {
-        int WEEK_COUNT = 50;
-        //XXX week_number starts from 0
-        week_number += 1;
-        List<WordPair> output_list = new ArrayList<WordPair>();
-        String word, meaning;
-        Log.i("WeekPairs", String.valueOf(week_number));
-        List<String> keys = new ArrayList<String>(word_map.keySet());
-        for (int i = WEEK_COUNT * week_number; i < WEEK_COUNT * (week_number + 1); ++i) {
-            word = keys.get(i);
-            meaning = word_map.get(word);
-            output_list.add(new WordPair(word, meaning));
-        }
-        //Check
-        for (WordPair wp : output_list)
-            Log.i("WeekPairs", wp.getWord() + wp.getMeaning());
-        return output_list;
-    }
-
 }
 // vim: set ts=4 sw=4 tw=0 et :
